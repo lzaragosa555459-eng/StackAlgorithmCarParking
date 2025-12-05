e1285097baef4910b9f648eaac6a688a70f2c241
Zil <l.zaragosa.555459@umindanao.edu.ph>
2025-12-06 00:03:46 +0800

    Refactor car management to use CarParking class

--- Main.java
+++ Main.java
@@ -3,12 +3,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        Stack s = new Stack();
+        CarParking s = new CarParking();
+
+        s.load();
 
         loop: while (true) {
+           
             System.out.println("--Menu--\n1]Add Car\n2]Pop Car\n3]Peek\n4]Display\n5]Exit\n\nChoose>>");
-            int choose = sc.nextInt();
+            int choose;
+
+            try{
+            choose = sc.nextInt();
+        }catch (InputMismatchException e){
+            System.out.println("Invalid Input! Enter number only.");
+            sc.nextLine();
+            continue;
+        }
+
+
 
             switch (choose) {
                 case 1:
@@ -45,9 +59,12 @@ public class Main {
                     System.out.println("Exit Program...");
                     break loop;
                 default:
-                    System.out.println("Invalid. Try again.");
+                    System.out.println("Invalid Input. Enter numbers' 1 to 5 only.");
                     break;
+              
             }
+        
+        
         }
         sc.close();
     }
