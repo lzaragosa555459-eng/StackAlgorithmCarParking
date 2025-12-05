ef8c52e683545379f0964ccdbe0c4d71e251a2c0
Zil <l.zaragosa.555459@umindanao.edu.ph>
2025-12-06 00:04:25 +0800

    Add CarParking.java file

---
+++ CarParking.java
@@ -0,0 +1,129 @@
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class CarParking {
+    private int size = 5;
+    private Object[][] stack = new Object[size][2];
+    private int top = -1;
+    private Object[][] temp = new Object[stack.length][stack[0].length];
+
+
+
+
+
+    
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == size - 1;
+    }
+
+    public void push(int car, String name) {
+        if (isFull()) {
+            System.out.println("Car Parking is Already full!");
+        } else {
+            stack[++top][0] = car;
+            stack[top][1] = name;
+            System.out.println("Plate: " + car + "\nName: " + name);
+            save(car, name);
+        }
+    }
+
+    public void pop(int moveCar) {
+		if(isEmpty()) {
+			System.out.println("Empty Cars! Nothing to move");
+		} else {
+			int index = 0;
+			
+			for(int i=0;i<stack.length;i++) {
+				if(i != moveCar) {
+					temp[index++]=stack[i];
+				}
+			}
+			top--;
+			
+			for(int i=0;i<stack.length;i++) {
+				for(int j=0;j < stack[i].length;j++) {
+					
+					stack[i][j] = temp[i][j];
+				}
+			}
+			update();
+		}
+	}
+
+    public void peek() {
+        if (isEmpty()) {
+            System.out.println("Empty Car parking. Nothing to peek here...");
+        } else {
+            System.out.println("Plate Number: " + stack[top][0]);
+            System.out.println("Owner Name: " + stack[top][1]);
+        }
+    }
+
+    public void display() {
+        if (isEmpty()) {
+            System.out.println("Car parking is empty");
+        } else {
+            System.out.println("--Car Parked for today--");
+            for (int i = 0; i <= top; i++) {
+                System.out.print(Arrays.deepToString(stack[i]) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public int gettop() {
+        return top;
+    }
+
+    private void save(int carPlate, String ownerName) {
+        String fileName = "parking_log.csv";
+        try (FileWriter writer = new FileWriter(fileName, true)) {
+
+            writer.write(carPlate + "," + ownerName + "\n");
+        } catch (IOException e) {
+            System.err.println("Error writing to log file: " + e.getMessage());
+        }
+    }
+
+    public void load(){
+        try(BufferedReader reader = new BufferedReader(new FileReader("parking_log.csv"))){
+        
+            String line;
+
+            while((line = reader.readLine()) != null){
+            
+            String[] values = line.split(",");
+            stack[++top][0] = values[0];
+            stack[top][1] = values[1];
+                
+            }
+            
+            
+
+                }catch(IOException e){
+                    System.out.println("There is something wrong reading your file");
+                }
+    }
+
+    public void update(){
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("parking_log.csv"))){
+
+            for (int i = 0; i <= top; i++) {
+                writer.write(stack[i][0] + "," + stack[i][1] + "\n");
+            }
+
+        } catch (IOException e){
+            System.out.println("Error! There is something wrong with updating the file.");
+        }
+    }
+}
